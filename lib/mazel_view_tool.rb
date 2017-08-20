03f586d3226306bc13239514ef280a29f56d04e2
Sixdes <ytxmailg@gmail.com>
2017-08-20 09:34:18 +0800

    gem tool initial commit

---
+++ lib/mazel_view_tool.rb
@@ -0,0 +1,6 @@
+require "mazel_view_tool/version"
+require "mazel_view_tool/renderer"
+
+module MazelViewTool
+  # Your code goes here...
+end
---
+++ lib/mazel_view_tool/version.rb
@@ -0,0 +1,3 @@
+module MazelViewTool
+  VERSION = "0.1.0"
+end
